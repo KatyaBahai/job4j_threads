534f97d9cda22820c84816e10765181952863bc8
KatyaBahai <katyxa-two@yandex.ru>
2023-10-24 19:37:20 +0600

    practicing stash/unstash

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -7,7 +7,7 @@ package ru.job4j;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Making new changes after stashing the previous ones");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
