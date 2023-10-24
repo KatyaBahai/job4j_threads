80410b825b78d6ee761988005a57591a2609cc92
KatyaBahai <katyxa-two@yandex.ru>
2023-10-24 19:38:47 +0600

    3.4. Временное хранение. git stash. [#504832]

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -11,7 +11,7 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("Practising git stash / git unstash");
 
     }
 
