65caad61da9a04cdb740399e526be6d0c8114d55
KatyaBahai <katyxa-two@yandex.ru>
2023-10-29 17:42:28 +0600

    3. Состояние нити. [#229175]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -6,14 +6,12 @@ public class ThreadState {
                 () -> { });
         System.out.println(first.getState());
         first.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getName());
-        }
         Thread second = new Thread(
                 () -> { });
         System.out.println(second.getState());
         second.start();
-        while (second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName());
             System.out.println(second.getName());
         }
         System.out.println(first.getState());
