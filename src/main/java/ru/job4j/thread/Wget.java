8115c4b924555114a7bc311c8688eea6f26840b1
KatyaBahai <katyxa-two@yandex.ru>
2023-11-01 15:12:05 +0600

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -6,35 +6,44 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String destFile;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String destFile) {
         this.url = url;
         this.speed = speed;
+        this.destFile = destFile;
     }
 
     @Override
     public void run() {
         var startAt = System.currentTimeMillis();
-        File file = new File("tmp.xml");
+        File file = new File(destFile);
         try (var in = new URL(url).openStream();
              var out = new FileOutputStream(file)) {
             System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
             var dataBuffer = new byte[512];
             int bytesRead;
+            int totalBytesRead = 0;
+            double totalTimeInMsec = 0;
             while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
-                var downloadAt = System.nanoTime();
+                long downloadAt = System.nanoTime();
                 out.write(dataBuffer, 0, bytesRead);
-                double writtenAt = (System.nanoTime() - downloadAt) / 1000000.0;
-                long currentSpeedBytePerMsecond = (long) (bytesRead / writtenAt);
-                System.out.println("Read " + bytesRead + " bytes : " + currentSpeedBytePerMsecond + " mSec.");
-                long diff = currentSpeedBytePerMsecond / speed;
-                if (diff > 1) {
+                double writtenAt = (System.nanoTime() - downloadAt) / 1_000_000.0;
+                totalTimeInMsec += writtenAt;
+                totalBytesRead += bytesRead;
+                System.out.println(totalBytesRead + " - " + totalTimeInMsec);
+                if (totalBytesRead >= speed && totalTimeInMsec < 1) {
                     try {
-                        Thread.sleep(diff);
+                        Thread.sleep((long) totalTimeInMsec);
+                        System.out.println("pause: " + totalTimeInMsec);
+                        totalBytesRead = 0;
+                        totalTimeInMsec = 0;
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -49,7 +58,8 @@ public class Wget implements Runnable {
         paramValidate(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String destFile = args[2];
+        Thread wget = new Thread(new Wget(url, speed, destFile));
         wget.start();
         wget.join();
     }
@@ -58,28 +68,33 @@ public class Wget implements Runnable {
         if (args.length == 0) {
             throw new IllegalArgumentException("Root folder is null. Use ROOT_FOLDER");
         }
-            if (args.length > 2) {
-                throw new IllegalArgumentException("Root folder must have 2 parameters. Use ROOT_FOLDER");
-            }
-            if (args.length < 2) {
-                throw new IllegalArgumentException("Parameter is missing. Use ROOT-FOLDER");
-            }
-            try {
-                URL url = new URL(args[0]);
-            } catch (MalformedURLException ex) {
-                throw new InvalidPathException(args[0], "The 1 parameter in Root Folder is not a url.");
-            }
-            if (!isInteger(args[1])) {
-                    throw new IllegalArgumentException("The 2 parameter should be an integer. Use ROOT-FOLDER");
-                }
-            }
-
-        private static boolean isInteger(String str) {
-            try {
-                Integer.parseInt(str);
-                return true;
-            } catch (NumberFormatException e) {
-                return false;
-            }
+        if (args.length > 3) {
+            throw new IllegalArgumentException("Root folder must have 3 parameters. Use ROOT_FOLDER");
         }
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Parameter is missing. Use ROOT-FOLDER");
+        }
+        try {
+            URL url = new URL(args[0]);
+        } catch (MalformedURLException ex) {
+            throw new IllegalArgumentException("The 1 parameter in Root Folder is not a url.");
+        }
+        if (!isInteger(args[1])) {
+            throw new IllegalArgumentException("The 2 parameter should be an integer.");
+        }
+        try {
+            Path destFile = Paths.get(args[2]);
+        } catch (InvalidPathException ex) {
+            throw new InvalidPathException(args[2], "The 3 parameter in Root Folder is not a path.");
+        }
+    }
+
+    private static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
