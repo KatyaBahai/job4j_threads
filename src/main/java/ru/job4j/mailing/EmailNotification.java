750a2d7512cb8a75093b22faf4308571d9987d4c
KatyaBahai <katyxa-two@yandex.ru>
2023-12-29 10:03:46 +0600

    2. ExecutorService рассылка почты. [#63097]

--- src/main/java/ru/job4j/mailing/EmailNotification.java
+++ src/main/java/ru/job4j/mailing/EmailNotification.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
-    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
         String subject = String.format("Notification %s to email %s", user.name(), user.email());
@@ -14,6 +14,13 @@ public class EmailNotification {
 
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void send(String subject, String body, String email) {
